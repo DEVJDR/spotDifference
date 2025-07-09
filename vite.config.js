6e5053c194bc45c9a3d99a2abc91a8bce2de21fe
DEVJDR <129095473+DEVJDR@users.noreply.github.com>
2025-07-09 20:24:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-const react = require('@vitejs/plugin-react-swc');
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-/** @type {import('vite').UserConfig} */
-module.exports = {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-};
+});
